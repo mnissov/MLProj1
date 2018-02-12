186be991a7c75b49c3cbc243efce3eca43948d9b
Morten Nissov <s163962@student.dtu.dk>
2018-02-12 12:05:38 +0100

    Swapping to Johan

--- MLProj1.py
+++ MLProj1.py
@@ -9,8 +9,17 @@ import xlrd
 import pandas as pd
 
 # Load xls sheet with data
-dataset = xlrd.open_workbook('wage2.xls').sheet_by_index(0)
-data = pd.get_dummies(dataset)
+#dataset = xlrd.open_workbook('wage2.xls').sheet_by_index(0)
+#data = pd.get_dummies(dataset)
 
-dataframe = pd.read_excel('wage2.xls', header = None)
-df
+
+df = pd.read_excel('wage2.xls', header = None)
+n = df.cou
+print(n)
+#index = list(range(n))
+df.columns = ['wage', 'hours', 'iq', 'kww', 'educ', 'exper', 'tenure', 'age', 'married', 'black', 'south', 'urban', 'sibs', 'brthord', 'meduc', 'feduc', 'lwage']
+#df.set_index(index, inplace=True)
+     
+df.dropna(inplace=True)
+
+#print(df.head(12))
