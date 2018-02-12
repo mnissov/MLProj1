7bd3d65dc6ea550cbfa20959365a20bac02d341a
Unknown <emma.demarecaux@ensta-paristech.fr>
2018-02-12 11:16:37 +0100

    import MLProj1.py

---
+++ MLProj1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 09:46:06 2018
+
+@author: Emma
+"""
+import numpy as np
+import xlrd
+
+# Load xls sheet with data
+dataset = xlrd.open_workbook('wage2.xls')
