180cc811c9a3f75e72a10398cf799ee47bd10a5f
Unknown <emma.demarecaux@ensta-paristech.fr>
2018-02-12 11:37:23 +0100

    modifications MLProjet.py

--- MLProj1.py
+++ MLProj1.py
@@ -6,6 +6,11 @@ Created on Mon Feb 12 09:46:06 2018
 """
 import numpy as np
 import xlrd
+import pandas as pd
 
 # Load xls sheet with data
-dataset = xlrd.open_workbook('wage2.xls')
+dataset = xlrd.open_workbook('wage2.xls').sheet_by_index(0)
+data = pd.get_dummies(dataset)
+
+dataframe = pd.read_excel('wage2.xls', header = None)
+df
