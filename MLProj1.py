41258d9e8d1891491cbcd15612135f9ae3ce178d
Morten Nissov <s163962@student.dtu.dk>
2018-02-18 13:35:52 +0100

    playing with pandas

--- MLProj1.py
+++ MLProj1.py
@@ -13,16 +13,22 @@ import pandas as pd
 #data = pd.get_dummies(dataset)
 
 
-df = pd.read_excel('wage2.xls', header = None)
+df = pd.read_excel('modified.xls', header = None)
 n = len(df.index)
-print(n)
+#print(n)
 #index = list(range(n))
-df.columns = ['wage', 'hours', 'iq', 'kww', 'educ', 'exper', 'tenure', 'age', 'married', 'black', 'south', 'urban', 'sibs', 'brthord', 'meduc', 'feduc', 'lwage']
+#df.columns = ['wage', 'hours', 'iq', 'educ', 'exper', 'tenure', 'age', 'black']
 df.reset_index()
 df.reindex(index=range(0,n))
-
-
      
 df.dropna(inplace=True)
+dfMatrix = df.as_matrix()
 
-print(df.head(12))
+#print(dfMatrix)
+
+y = dfMatrix[1:,0]
+yMatrix = np.mat(y)
+
+X = np.mat(np.empty((n,7))
+for i in range(7):
+    X[i,:] = np.matrix(dfMatrix[)
