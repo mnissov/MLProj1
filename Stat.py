9d650ae1c280fb3b60579a0b2638cb8da687a502
Morten Nissov <s163962@student.dtu.dk>
2018-02-20 17:06:41 +0100

    Final update

--- Stat.py
+++ Stat.py
@@ -20,17 +20,25 @@ sim1 = np.zeros(7)
 
 for i in range(0,7):
     means[i] = X[:,i].mean()
-    sd[i] = X[:,i].std(ddof=1)
     maxes[i] = X[:,i].max()
     mins[i] = X[:,i].min()
     
 myvar=np.zeros(7)
 for j in range (0,7):
-    for i in range(0,934):
+    for i in range(0,935):
         myvar[j] += ((X[i,j]-means[j]) ** 2)/934
+        
+for i in range(0,7):
+    sd[i] = math.sqrt(myvar[i])
         
 cov = np.zeros((7,7))
 for i in range (0,7):
     for j in range(0,7):
-        for k in range(0,934):
-            cov[i,j] += ((X[k,i]-means[i]) * (X[k,j] - means[j]))/935
+        for k in range(0,935):
+            cov[i,j] += ((X[k,i]-means[i]) * (X[k,j] - means[j]))/934
+            
+corr = np.zeros((7,7))
+
+for i in range (0,7):
+    for j in range(0,7):
+        corr[i,j] = (cov[i,j]) / (sd[i] * sd[j])
